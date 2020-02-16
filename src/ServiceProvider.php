edc922154e7bda0d473d69fbf8d29409442ff59d
Dave Rogers <drogers@dealerinspire.com>
2020-02-16 08:15:39 -0600

    Setup update
    Update directories and filenames for the setup

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -18,19 +18,23 @@ class ServiceProvider extends IlluminateServiceProvider
             return;
         }
 
-        $stub      = __DIR__ . '/database/migrations/';
-        $target    = database_path('migrations') . '/';
+        $migrationsDirectory = __DIR__ . '/database/migrations/';
+        $configDirectory = __DIR__ . '/config/';
+        $targetDirectory = database_path('migrations') . '/';
 
         $this->publishes([
-            $stub . 'create_microblog_posts_table.php' => $target . date('Y_m_d_His', time()) . '_create_microblog_posts_table.php',
+            $migrationsDirectory . '2019_02_14_090000_create_microblog_posts_table.php'
+                => $targetDirectory . date('Y_m_d_His', time()) . '_create_microblog_posts_table.php',
         ], 'migrations');
 
         $this->publishes([
-            $stub . 'create_microblog_journals_table.php' => $target . date('Y_m_d_His', time()) . '_create_microblog_journals_table.php',
+            $migrationsDirectory . '2019_02_14_090000_create_microblog_journals_table.php'
+                => $targetDirectory . date('Y_m_d_His', time()) . '_create_microblog_journals_table.php',
         ], 'migrations');
 
         $this->publishes([
-            __DIR__ . '/config/microblog.php' => config_path('microblog.php'),
+            $configDirectory . 'microblog.php'
+                => config_path('microblog.php'),
         ], 'config');
     }
 
